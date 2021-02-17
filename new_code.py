1ad95ce10895cde7b390e212da59d2af47ccfbf2
Pavitra Vasudevan <pavitravasudevan@gmail.com>
2021-02-17 12:24:43 -0500

    Added additional choices for chocolate variants

--- new_code.py
+++ new_code.py
@@ -5,8 +5,14 @@ print("Hello guys")
 def chocolate_lovers(choice):
     if choice.lower() == "dark":
         print("You have an amazing choice!!")
+    elif choice.lower() == "ruby":
+        print("The naturally pink tasty chocolate, really expensive though!!")
+    elif choice.lower() == "milk":
+        print("Tasty buttery milky!!")
+    elif choice.lower() == "white":
+        print("Too much sweet!!")
     else:
-        print("Well not a bad choice!!")
+        print("Please enter any four of the valid choice next time(dark/milk/white/ruby)")
+
 choice = ("Enter the type of chocolate you want to buy(dark/milk/white/ruby): ")
 chocolate_lovers(choice)
-    
