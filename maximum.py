0b677bcad86f5358d1611da1b0356f2b856ee62c
Pavitra Vasudevan <pavitravasudevan@gmail.com>
2021-02-17 14:27:47 -0500

    New file extended choices and print command

--- maximum.py
+++ maximum.py
@@ -5,3 +5,9 @@ print("Hello guys")
 choice = input("Enter 1st number: ")
 choice2 = input("Enter 1st number: ")
 print(choice)
+<<<<<<< HEAD
+=======
+print("#####")
+print(choice2)
+print("#####")
+>>>>>>> 9fbea5a (New file extended choices and print command)
